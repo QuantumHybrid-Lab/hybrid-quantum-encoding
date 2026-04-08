0c9486df3c016f352d8c2ada7ab1a34789790225
Emine <emine.gulmez444@gmail.com>
2026-04-09 00:38:46 +0300

    Added professional documentation and comments for presentation. Finalized baseline accuracy analysis with L2 normalization.

--- veri_analizi.py
+++ veri_analizi.py
@@ -16,12 +16,12 @@ feature_imp = pd.Series(selector.feature_importances_, index=X.columns)
 top_3_features = feature_imp.nlargest(3).index.tolist()
 print(f"\n--- SEÇİLEN 3 ÖZELLİK: {top_3_features} ---")
 
-# 3. L2 NORMALİZASYON (Amplitude Encoding için kareler toplamı = 1 yapar)
+# 3. L2 NORMALİZASYON (Amplitude Encoding (genlik kodlama)için kareler toplamı = 1 yapar)
 X_selected = X[top_3_features]
 scaler = Normalizer(norm='l2') 
 X_scaled = scaler.fit_transform(X_selected)
 
-# 4. YENİ BASELINE (SVM)
+# 4. YENİ BASELINE (SVM) %80 eğitim %20 test olarak böl ve eğit
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 model = SVC(kernel='rbf', C=10)
 model.fit(X_train, y_train)
